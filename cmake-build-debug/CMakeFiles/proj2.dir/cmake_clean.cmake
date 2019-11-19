0eef479cdecb100d47e6730f0bb6e5b9e0d6a9ab
shirmo <shirmolina@gmail.com>
2019-11-19 14:29:35 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/proj2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proj2.dir/main.c.o"
+  "proj2.pdb"
+  "proj2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/proj2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
